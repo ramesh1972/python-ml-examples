191f735dfb82aeb1d3bd36c07cc3dcc9f61043c4
ramesh1972 <62056258+ramesh1972@users.noreply.github.com>
2024-01-08 11:37:44 +0530

    inti4

---
+++ fine-tuning-python/upload_files_fine_tune.py
@@ -0,0 +1,102 @@
+# imports
+import pandas as pd
+import tiktoken
+import openai
+import json
+import signal
+import datetime
+
+openai.api_key = "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"
+
+# embedding model parameters	
+GPT_MODEL = "gpt-3.5-turbo"
+
+# upload files to Open AI
+training_file_id = openai.files.create(
+  file=open("./data/training_data.jsonl", "rb"),
+  purpose='fine-tune',
+).id
+
+validation_file_id = openai.files.create(
+  file=open("./data/validation_data.jsonl", "rb"),
+  purpose='fine-tune'
+).id
+
+print(f"Training File ID: {training_file_id}")
+print(f"Validation File ID: {validation_file_id}")
+print("-------------------------")
+
+# create fine tune job in open AI
+create_args = {
+	"training_file": str(training_file_id),
+	"validation_file": str(validation_file_id),
+	"model": "davinci",
+	"batch_size": 3,
+	"learning_rate_multiplier": 0.3
+}
+
+
+response = openai.fine_tunes.create(**create_args)
+job_id = response.id
+status = response.status
+
+print(f'Fine-tunning model with jobID: {job_id}.')
+print(f"Training Response: {response}")
+print(f"Training Status: {status}")
+print("-------------------------")
+
+""" # stream events from the fine tune job created in Open AI
+def signal_handler(sig, frame):
+	status = openai.FineTune.retrieve(job_id).status
+	print(f"Stream interrupted. Job is still {status}.")
+	return
+
+print("-------------------------")
+print(f'Streaming events for the fine-tuning job: {job_id}')
+signal.signal(signal.SIGINT, signal_handler) """
+
+# try:
+#for event in response:
+ #   print(event)
+
+# except Exception:
+# 	print("Stream interrupted (client disconnected).")
+
+print("-------------------------")
+
+import time
+
+# print job status
+status = openai.fine_tunes.retrieve(job_id).status
+if status not in ["succeeded", "failed"]:
+	print(f'Job not in terminal status: {status}. Waiting.')
+	while status not in ["succeeded", "failed"]:
+		time.sleep(2)
+		status = openai.fine_tunes.retrieve(job_id).status
+		print(f'Status: {status}')
+else:
+	print(f'Finetune job {job_id} finished with status: {status}')
+
+print("-------------------------")
+print('Checking other finetune jobs in the subscription.')
+result = openai.fine_tunes.list()
+print(f'Found {len(result.data)} finetune jobs.')
+
+# Retrieve the finetuned model
+print("-------------------------")
+
+fine_tuned_model = openai.fine_tunes.retrieve(job_id).fine_tuned_model
+print("fine_tuned_model is " + fine_tuned_model)
+print("-------------------------")
+
+# prompt OpenAI to return fro uploaded files
+new_prompt = "Capital of France?"
+answer = openai.completions.create(
+  model=fine_tuned_model,
+	prompt=new_prompt,
+	max_tokens=100,
+)
+
+print("-------------------------")
+print("prompt=" + new_prompt)
+print(answer.choices[0].text)
